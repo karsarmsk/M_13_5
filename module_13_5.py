cd545c5ae42b03de869915a19183f602c066ed7f
Сергей Карасев <karasev-s@internet.ru>
2024-09-24 22:50:22 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = ""
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+button = KeyboardButton(text = 'Рассчитать')
+button2 = KeyboardButton(text = 'Информация')
+kb = ReplyKeyboardMarkup(resize_keyboard=True).row(button, button2)
+
+# kb.row(button)  # в клавиатуру добавляем кнопку,каждый add добавляет кнопку на новый ряд
+# kb.row(button2)
+
+@dp.message_handler(commands = ['start'])
+async def start(message):
+    await message.answer("Привет!", reply_markup = kb)
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+   await state.update_data(growth=int(message.text))
+   await message.answer('Введите свой вес:')
+   await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f"Ваша дневная норма калорий: {calories} ккал")
+    await state.finish()
+
+@dp.message_handler()  # если () оле пустое реагирует на все
+async def all_message(message):
+#    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
